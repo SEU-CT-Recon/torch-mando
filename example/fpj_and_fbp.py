c01e722c113cbb5ebaff5445f4f1f34e6a0f62f9
CandleHouse <769316285@qq.com>
2022-10-29 20:14:26 +0800

    Bugs fixed: short scan judge

--- example/fpj_and_fbp.py
+++ example/fpj_and_fbp.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from crip.io import imwriteTiff
-from torch_mango import RadonFanbeam
+from torch_mango import MangoFanbeam
 
 device = torch.device('cuda')
 img = np.load("shepplogan.npy")  # 512*512
@@ -12,7 +12,7 @@ totalAngle = 360
 detEleCount = 300
 detEleSize = 1
 imgPixelSize = 0.5
-radon = RadonFanbeam(750, 800, views, detEleSize, 1, 0, totalAngle, imgDim, imgPixelSize, 0.2, views, detEleCount,
+radon = MangoFanbeam(750, 800, views, detEleSize, 1, 0, totalAngle, imgDim, imgPixelSize, 0.2, views, detEleCount,
                      kernelId, 1, 0, 0, 0, 0, True)
 
 
