2306bcc8e112e5e2b16ae313853e5d02f744a306
CandleHouse <769316285@qq.com>
2022-11-09 11:51:17 +0800

    update example

--- example/fpj_and_fbp.py
+++ example/fpj_and_fbp.py
@@ -17,11 +17,12 @@ detEleCount = 650
 detEleSize = 1
 imgPixelSize = 0.5
 cfg = MandoFanBeamConfig(imgDim=imgDim, pixelSize=imgPixelSize, sid=750, sdd=1250, detEltCount=detEleCount,
-                         detEltSize=detEleSize, views=views,reconKernelEnum=KERNEL_RAMP, reconKernelParam=1,
+                         detEltSize=detEleSize, views=views, reconKernelEnum=KERNEL_RAMP, reconKernelParam=1,
                          fovCrop=False)
 
+img1, img2, img3, img4 = img, img.T, np.fliplr(img), np.flipud(img)
 with torch.no_grad():
-    x = torch.FloatTensor(np.array([img, img.T]))  # B, H, W
+    x = torch.FloatTensor(np.array([[img1, img2], [img3, img4]]))  # B, C, H, W
     x = x.to(device)
 
     sinogram = MandoFanbeamFpj(x, cfg)
